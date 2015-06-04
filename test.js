3cfbbae1f58de27519831baca2b1d4eafeda0211
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-04 12:47:43 +0200

    Revert "Ignore demo-specific statements from coverage"
    This reverts commit dd924121f5e7856217ad49a61107d73d74adcfaa.

--- test.js
+++ test.js
@@ -33,20 +33,14 @@ test('Does what the demo says', (is) => {
   );
 
   const myTemplate = ({data}, index) => ({data,
-    output: `${data.isPrivate ?
-      /* istanbul ignore next */ 'Private' :
-      'Public'
-    } n° ${index + 1}\n`
+    output: `${data.isPrivate ? 'Private' : 'Public'} n° ${index + 1}\n`
   });
 
   is.deepEqual(
     doxie([
       (comments) => comments.filter(myFilter),
       (comments) => comments.map(myTemplate),
-      (comments) => comments.map(({output}) => (
-        output ||
-        /* istanbul ignore next */ ''
-      )).join('')
+      (comments) => comments.map(({output}) => output || '').join('')
     ])(myData),
     'Public n° 1\nPublic n° 2\n',
     'outputting docs for humans'

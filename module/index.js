1bb3c990c768e30f0c799a10d69d838c846df0ae
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-10 17:45:23 +0200

    Write to IO streams

--- module/index.js
+++ module/index.js
@@ -5,7 +5,29 @@ export default (
   plugins,
   {stdout = null, stderr = null} = {}
 ) => {
-  const transform = plugins.reduce(pipe, identity);
+  const writeOutput = (
+    stdout && typeof stdout.write === 'function' ?
+    stdout.write.bind(stdout) :
+    null
+  );
+
+  const writeError = (
+    stderr && typeof stderr.write === 'function' ?
+    stderr.write.bind(stderr) :
+    null
+  );
+
+  const transform = plugins
+    .map((plugin) => (...args) => {
+      let result = plugin(...args);
+      let {error, output} = result;
+      if (output && writeOutput) writeOutput(output);
+      if (error && writeError) writeError(error);
+      return result;
+    })
+    .reduce(pipe, identity)
+  ;
+
   return (doxOutput) => {
     const input = {
       chunks: doxOutput.map((data) => ({data})),

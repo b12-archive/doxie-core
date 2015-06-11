6a268204dd4096b49f61f2ae4caa04b1ce69d0a1
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-11 16:16:46 +0200

    Check plugin output

--- module/index.js
+++ module/index.js
@@ -1,4 +1,11 @@
 const pipe = require('1-liners/pipe');
+const {cyan} = require('chalk');
+const tinyError = require('tiny-error');
+const {isArray} = Array;
+
+const pluginError = tinyError({
+  prefix: `${cyan('[doxie]')} Got invalid data from a plugin. `,
+});
 
 export default (
   plugins,
@@ -19,7 +26,25 @@ export default (
   return plugins
     .map((plugin) => (...args) => {
       let result = plugin(...args);
-      let {error, output} = result;
+
+      if (!result || typeof result !== 'object') throw pluginError(
+        'It should have returned a data object.'
+      );
+
+      let {version, chunks, error, output} = result;
+
+      if (typeof version !== 'number') throw pluginError(
+        'The returned data should contain a `{Number} version`'
+      );
+
+      if (version !== 1) throw pluginError(
+        'The returned `version` should equal `1` in doxie <2.0.0'
+      );
+
+      if (!isArray(chunks)) throw pluginError(
+        'The returned data should contain an `{Array} chunks`'
+      );
+
       if (output && writeOutput) writeOutput(output);
       if (error && writeError) writeError(error);
       return result;
